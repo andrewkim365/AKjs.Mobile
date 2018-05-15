f038a166ef989f00b18ad59a8f409576f30f7d00
Andrew Kim <andrewkim365@qq.com>
2018-05-15 10:45:58 +0800

    Update Andrew_WebToast.js

--- js/plugin/Andrew_WebToast.js
+++ js/plugin/Andrew_WebToast.js
@@ -1,56 +1,58 @@
-﻿/*
-Modification Date: 2018-05-12
+/*
+Modification Date: 2018-05-15
 Coding by Andrew.Kim (E-mail: andrewkim365@qq.com)
 */
 /*-----------------------------------------------Andrew_WebToast-------------------------------------------*/
 function ak_webToast(){
     var dcfg = {
-        msg: "",
-        postion: "bottom",
+        message: "",
+        position: "bottom",
+        mask: "mask",
         time: 1000
     };
     var Andrew_WebToast = ".ak-webtoast";
     var sub_Andrew_WebToast = Andrew_WebToast.substring(1,Andrew_WebToast.length);
     var len = arguments.length;
-    var arg0 = arguments[0];
-    if (arguments.length > 0) {
-        dcfg.msg = arguments[0];
-        dcfg.msg = arg0;
 
-        var arg1 = arguments[1];
+    if (len > 0) {
+        var arg0 = arguments[0]; //message
+        var arg1 = arguments[1]; //position
+        var arg2 = arguments[2]; //mask
+        var arg3 = arguments[3]; //time
         var regx = /(bottom|top|middle)/i;
-        var regy = /()/i;
+        var regy = /(mask)/i;
         var numRegx = /[1-9]\d*/;
-        if (regx.test(arg1)) {
-            dcfg.postion = arg1;
-        } else if (regy.test(arg1)) {
-            dcfg.mask = arg1;
-        } else if (numRegx.test(arg1)) {
-            dcfg.time = arg1;
+        //debugger;
+        //console.log(regy.test(arg2))
+        if (arg0) {
+            dcfg.message = arg0;
         }
-
-        var arg2 = arguments[2];
-        var numRegx = /[1-9]\d*/;
-        if (numRegx.test(arg2)) {
+        if (regx.test(arg1)) {
+            dcfg.position = arg1;
+        }
+        if (regy.test(arg2) || arg2=="") {
+            dcfg.mask = arg2;
+            if (numRegx.test(arg3)) {
+                dcfg.time = arg3;
+            }
+        } else {
             dcfg.time = arg2;
         }
-        if (regy.test(arg2)) {
-            dcfg.mask = arg2;
-        }
+
     }
-    if ("mask" == dcfg.mask) {
-        var ret = "<div class='"+sub_Andrew_WebToast+" animated fadeIn'><div class='ak-mask'></div><h3>" + dcfg.msg + "</h3></div>";
+    if ("mask" == arg2) {
+        var ret = "<div class='"+sub_Andrew_WebToast+" animated fadeIn'><div class='ak-mask'></div><h3>" + dcfg.message + "</h3></div>";
     } else {
-        var ret = "<div class='"+sub_Andrew_WebToast+" animated fadeIn'><h3>" + dcfg.msg + "</h3></div>";
+        var ret = "<div class='"+sub_Andrew_WebToast+" animated fadeIn'><h3>" + dcfg.message + "</h3></div>";
     }
     if ($(Andrew_WebToast).length <= 0) {
         $("body").append(ret);
     } else {
         $(Andrew_WebToast).css("left", "");
-        if ("mask" == dcfg.mask) {
-            ret = "<div class='ak-mask'></div><h3>" + dcfg.msg + "</h3>";
+        if ("mask" == arg2) {
+            ret = "<div class='ak-mask'></div><h3>" + dcfg.message + "</h3>";
         } else {
-            ret = "<h3>" + dcfg.msg + "</h3>";
+            ret = "<h3>" + dcfg.message + "</h3>";
         }
         $(Andrew_WebToast).html(ret);
     }
@@ -58,13 +60,13 @@ function ak_webToast(){
         ww = $(window).width();
     $(Andrew_WebToast).fadeIn();
     $(Andrew_WebToast).css("left", (ww - w) / 2 - 20);
-    if ("bottom" == dcfg.postion) {
+    if ("bottom" == dcfg.position) {
         $(Andrew_WebToast).css("bottom", 50);
         $(Andrew_WebToast).css("top", "");
-    } else if ("top" == dcfg.postion) {
+    } else if ("top" == dcfg.position) {
         $(Andrew_WebToast).css("bottom", "");
         $(Andrew_WebToast).css("top", 50);
-    } else if ("middle" == dcfg.postion) {
+    } else if ("middle" == dcfg.position) {
         $(Andrew_WebToast).css("bottom", "");
         $(Andrew_WebToast).css("top", "");
         var h = $(Andrew_WebToast).height(),
