518352ffa4be471a19af259a711e1008ade21996
Andrew Kim <andrewkim365@qq.com>
2018-07-11 16:11:27 +0800

    Add files via upload

--- js/plugin/Andrew_Popupwin.js
+++ js/plugin/Andrew_Popupwin.js
@@ -1,5 +1,5 @@
 ﻿/*
-Modification Date: 2018-07-09
+Modification Date: 2018-07-11
 Coding by Andrew.Kim (E-mail: andrewkim365@qq.com)
 */
 /*-----------------------------------------------Andrew_Popupwin----------------------------------------*/
