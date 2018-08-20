6bd4bbba624611f9e434dd105e5ffef7632bd338
andrewkim365@qq.com <andrewkim365@qq.com>
2018-08-20 15:22:43 +0800

    Add files via upload

--- js/akjs.mobile.js
+++ js/akjs.mobile.js
@@ -1390,13 +1390,13 @@ function AKjs_Plugin(setting,css) {
             type:'GET',
             url: js_folder+"plugin/"+setting+".js?akjs="+new Date().getTime(),
             async: false,
-            cache: false,
+            cache: true,
             dataType:'script'
         });
         if (css) {
             var css_url = js_folder + "plugin/css/" + setting + ".css";
-            $("head").find("link").filter("#"+setting).remove();
-            $("head").find("link:first").before("<link rel='stylesheet' type='text/css' id='"+setting+"' href='"+css_url+"?akjs="+new Date().getTime()+"' />");
+            $("head").find("style").filter("#"+setting).remove();
+            $("head").find("link:first").before("<style type='text/css' id='"+setting+"'>@import url("+css_url+"?akjs="+new Date().getTime()+");</style>");
         }
     });
 }
