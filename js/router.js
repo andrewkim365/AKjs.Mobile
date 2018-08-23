0518568e3a58186a629d4082940aba9cde4a1d6e
andrewkim365@qq.com <andrewkim365@qq.com>
2018-08-23 10:19:39 +0800

    Add files via upload

--- js/router.js
+++ js/router.js
@@ -21,7 +21,6 @@
 */
 /*-----------------------------------------------AKjs_Router (路由全局设置）使用方法-------------------------------------------*/
 $(document).ready(function(){
-    AKjs_Include("css/theme.default.css"); //颜色相关样式文件引入（AKjs_Include是js文件中引入另一个js或css文件的功能）
     AKjs_Router({ //路由配置管理
         Router: true, //是否开启路由（开启路由URL中带#的路径访问页面不刷新页面形式跳转 (开启 true, 停用 false）
         FileFormat: ".html", //路由目录中的文件格式设置,该参数设置后data-href值里可以不写文件格式 （可设置html,php,aspx,jsp...等程序的文件名）
@@ -34,19 +33,22 @@ $(document).ready(function(){
                 AKjs_Location("/start"); //location.replace 跳转模式
             }
             if (!change) { //change是用于判断hash模式是否跳页
+                AKjs_Include("css/theme.default.css"); //颜色相关样式文件引入（AKjs_Include是js文件中引入另一个js或css文件的功能）
                 AKjs_Include("http://echarts.baidu.com/dist/echarts.min.js"); //Echarts插件全局引入（AKjs_Include是js文件中引入另一个js或css文件的功能）
                 AKjs_Include("js/data.js",true); //Json数据文件引入（AKjs_Include是js文件中引入另一个js或css文件的功能。后面加 true 代表优先级引入的意思（important））
                 AKjs_Include("js/plugin.js"); //功能插件按需引入（AKjs_Include是js文件中引入另一个js或css文件的功能）
             } else {
                 /*-----------------------------------------------AKjs_Loader 使用方法-------------------------------------------*/
                 if (AKjs_Params(1) != "start") { //通过AKjs_Params获取hash的第一个值后不执行下面loading效果
-                    AKjs_Loader({
-                        //ele: $("#ak-scrollview"), //是否使用局部遮挡层，使用请设置指定的局部元素 （不设置任何参数代表使用全部遮挡层）
-                        autoMode: true, //是否开启指定的时间后自动消失功能 (开启 true, 关闭 false）
-                        timeToHide: 1000, //毫秒时间设置 (automode必须开启才能有效)
-                        iconColor:"#ffffff", //图标颜色设置
-                        maskBG: false, //是否开启遮挡背景 (开启 true, 关闭 false）
-                        Loader:"load_2" //loading效果选择（load_1~7）
+                    $(function() {
+                        AKjs_Loader({
+                            //ele: $("#ak-scrollview"), //是否使用局部遮挡层，使用请设置指定的局部元素 （不设置任何参数代表使用全部遮挡层）
+                            autoMode: true, //是否开启指定的时间后自动消失功能 (开启 true, 关闭 false）
+                            timeToHide: 1000, //毫秒时间设置 (automode必须开启才能有效)
+                            iconColor:"#ffffff", //图标颜色设置
+                            maskBG: false, //是否开启遮挡背景 (开启 true, 关闭 false）
+                            Loader:"load_2" //loading效果选择（load_1~7）
+                        });
                     });
                 }
                 setTimeout(function() { //页面加载完5秒后执行
@@ -55,8 +57,9 @@ $(document).ready(function(){
                     }
                 },2000);
             }
+
+            /*-----------------------------------------------AKjs_Config (全局设置）使用方法-------------------------------------------*/
             $(function() {
-                /*-----------------------------------------------AKjs_Config (全局设置）使用方法-------------------------------------------*/
                 AKjs_Config({ //环境配置管理
                     MaskStyle: ["style3","opacity05"], //1.所有弹窗背景图案选择（样式style1~8）、2.遮挡层背景的透明度（opacity01~09）
                     Responsive: true, //是否开启文字大小按屏幕尺寸自适应变化，考虑到兼容平板电脑建议开启 (开启 true, 停用 false）
@@ -69,8 +72,10 @@ $(document).ready(function(){
                     Orientation: true, //是否开启应用只允许竖屏浏览 (使用 true, 不使用 false）
                     Prompt: "为了更好的视觉体验，请在竖屏下进行操作。" //应用横屏是提示文字 (必须开启Orientation的选项才能生效)
                 });
+            });
 
-                /*-----------------------------------------------AKjs_Menu (底部菜单图标设置）使用方法-------------------------------------------*/
+            /*-----------------------------------------------AKjs_Menu (底部菜单图标设置）使用方法-------------------------------------------*/
+            $(function() {
                 AKjs_Menu({ //底部菜单的图标以及文字样式变化设置
                     active_color: "c_title", //被选中的文字和图标的颜色
                     menu_icon: new Array( //每个菜单的默认显示图标设置 （为了正常的显示布局最多设置5个）
@@ -91,8 +96,10 @@ $(document).ready(function(){
                         //console.log(ele, index);
                     }
                 });
+            });
 
-                /*-----------------------------------------------AKjs_Responsive 使用方法-------------------------------------------*/
+            /*-----------------------------------------------AKjs_Responsive 使用方法-------------------------------------------*/
+            $(function() {
                 AKjs_Responsive({ //实时监听窗口大小变化的功能
                     resizeCallback: function (deviceW,deviceH,viewportW,viewportH) {
                         //console.log("屏幕宽度: "+deviceW);
@@ -101,60 +108,60 @@ $(document).ready(function(){
                         //console.log("实时窗口高度: "+viewportH);
                     }
                 });
-                /*
-                    AKjs_Location 使用方法：
-                    //url=跳转路径，{type=跳转类型（href,history,reload），time=延迟时间，router=页面切换效果（left,right）}
-
-                    AKjs_Location("url",{type:"", time: "", router:""}); //参数设置结构
-                    AKjs_Location("/start"); //location.replace 跳转模式(第二个参数默认识别time参数)
-                    AKjs_Location("/start",{type:"href"}); //location.href 跳转模式
-                    AKjs_Location("/",{type:"reload"}); //location.reload() 刷新当前页
-                    AKjs_Location("-1",{type:"history"}); //history.back(-1) 跳转返回上一页,也可以设置0，-2 等数值
-                    AKjs_Location("/start",{time:1000}); //location.replace 跳转模式 (延迟1秒后跳转)
-                    AKjs_Location("/start",{router:left}); //location.replace 跳转模式 (router参数是页面切换效果，left是左滑[返回效果]，right是右滑[进入效果])
-
-                    AKjs_getUrlParam &  AKjs_changeURLArg 使用方法：
-                    console.log("GET_ak: "+AKjs_getUrlParam('ak')); //获取URL中的参数值
-                    console.log(AKjs_changeURLArg(location.hash,"ak","change"+AKjs_getUrlParam('ak'))); //更改URL中的参数值
-
-                    AKjs_Unicode 使用方法：
-                    console.log(AKjs_Unicode("中文字符"));
-
-                    AKjs_setCookie & AKjs_getCookie & AKjs_delCookie 使用方法：
-                    AKjs_setCookie("username", user, 365); //设置cookie
-                    var user = AKjs_getCookie("username"); //获取cookie
-                    AKjs_delCookie(name) //删除cookie
-
-                    AKjs_htmlEncode & AKjs_htmlDecode 使用方法：
-                    AKjs_htmlEncode(str); //把TEXT转换HTML
-                    AKjs_htmlDecode(str); //把HTML转换TEXT
-
-                    AKjs_DateFormat & AKjs_FileFormat 使用方法：
-                    console.log(AKjs_DateFormat("2018/03/30 17:50","yyyy-MM-dd HH:mm"));
-                    console.log(AKjs_DateFormat(new Date(),"yyyy-MM-dd HH:mm"));
-                    AKjs_FileFormat(filename) //获取文件的扩展名
-                */
-
-                /*
-                    $(window).resize(function () {
-                        if ($(window).height() - $(this).height() > 140) {
-                            //安卓软键盘弹出
-                        } else {
-                            //安卓软键盘收起
-                        }
-                    });
-
-                    $(document).on('focusin', function () {
-                        //IOS软键盘弹出的事件处理
-                    });
-
-                    $(document).on('focusout', function () {
-                        //IOS软键盘收起的事件处理
-                    });
-
-                    document.activeElement.blur(); //强制隐藏软键盘
-                */
             });
+            /*
+                AKjs_Location 使用方法：
+                //url=跳转路径，{type=跳转类型（href,history,reload），time=延迟时间，router=页面切换效果（left,right）}
+
+                AKjs_Location("url",{type:"", time: "", router:""}); //参数设置结构
+                AKjs_Location("/start"); //location.replace 跳转模式(第二个参数默认识别time参数)
+                AKjs_Location("/start",{type:"href"}); //location.href 跳转模式
+                AKjs_Location("/",{type:"reload"}); //location.reload() 刷新当前页
+                AKjs_Location("-1",{type:"history"}); //history.back(-1) 跳转返回上一页,也可以设置0，-2 等数值
+                AKjs_Location("/start",{time:1000}); //location.replace 跳转模式 (延迟1秒后跳转)
+                AKjs_Location("/start",{router:left}); //location.replace 跳转模式 (router参数是页面切换效果，left是左滑[返回效果]，right是右滑[进入效果])
+
+                AKjs_getUrlParam &  AKjs_changeURLArg 使用方法：
+                console.log("GET_ak: "+AKjs_getUrlParam('ak')); //获取URL中的参数值
+                console.log(AKjs_changeURLArg(location.hash,"ak","change"+AKjs_getUrlParam('ak'))); //更改URL中的参数值
+
+                AKjs_Unicode 使用方法：
+                console.log(AKjs_Unicode("中文字符"));
+
+                AKjs_setCookie & AKjs_getCookie & AKjs_delCookie 使用方法：
+                AKjs_setCookie("username", user, 365); //设置cookie
+                var user = AKjs_getCookie("username"); //获取cookie
+                AKjs_delCookie(name) //删除cookie
+
+                AKjs_htmlEncode & AKjs_htmlDecode 使用方法：
+                AKjs_htmlEncode(str); //把TEXT转换HTML
+                AKjs_htmlDecode(str); //把HTML转换TEXT
+
+                AKjs_DateFormat & AKjs_FileFormat 使用方法：
+                console.log(AKjs_DateFormat("2018/03/30 17:50","yyyy-MM-dd HH:mm"));
+                console.log(AKjs_DateFormat(new Date(),"yyyy-MM-dd HH:mm"));
+                AKjs_FileFormat(filename) //获取文件的扩展名
+            */
+
+            /*
+                $(window).resize(function () {
+                    if ($(window).height() - $(this).height() > 140) {
+                        //安卓软键盘弹出
+                    } else {
+                        //安卓软键盘收起
+                    }
+                });
+
+                $(document).on('focusin', function () {
+                    //IOS软键盘弹出的事件处理
+                });
+
+                $(document).on('focusout', function () {
+                    //IOS软键盘收起的事件处理
+                });
+
+                document.activeElement.blur(); //强制隐藏软键盘
+            */
         },
         error:function (hash) { //请求加载页面失败后的回调
             if (hash) { //获取hash的参数值，当前的判断是hash有值的情况
