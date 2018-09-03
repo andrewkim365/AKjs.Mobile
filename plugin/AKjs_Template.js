5838990ba54a94282dc1e99a5615557e0b01e40a
andrewkim365@qq.com <andrewkim365@qq.com>
2018-09-03 15:18:54 +0800

    Add files via upload

--- plugin/AKjs_Template.js
+++ plugin/AKjs_Template.js
@@ -1,10 +1,20 @@
 /*
-Modification Date: 2018-08-09
+Modification Date: 2018-09-03
 Coding by Andrew.Kim (E-mail: andrewkim365@qq.com)
 */
 (function($, undefined) {
-    var oldManip = $.fn.domManip, AKjs_TemplateItmAtt = "_Template_Item",
-        newTemplate_Items = {}, wrappedItems = {}, appendToTemplate_Items, topTemplate_Item = { key: 0, data: {} }, itemKey = 0, cloneIndex = 0, stack = [];
+    var oldManip = $.fn.domManip,
+        tmplItmAtt = "_tmplitem",
+        newTmplItems = {},
+        wrappedItems = {},
+        appendToTmplItems,
+        topTmplItem = {
+            key: 0,
+            data: {}
+        },
+        itemKey = 0,
+        cloneIndex = 0,
+        stack = [];
     var regex = {
         sq_escape: /([\\'])/g,
         sq_unescape: /\\'/g,
@@ -13,13 +23,13 @@ Coding by Andrew.Kim (E-mail: andrewkim365@qq.com)
         shortcut_replace: /\$\{([^\}]*)\}/g,
         lang_parse: /\{\%(\/?)(\w+|.)(?:\(((?:[^\%]|\%(?!\}))*?)?\))?(?:\s+(.*?)?)?(\(((?:[^\%]|\%(?!\}))*?)\))?\s*\%\}/g,
         old_lang_parse: /\{\{(\/?)(\w+|.)(?:\(((?:[^\}]|\}(?!\}))*?)?\))?(?:\s+(.*?)?)?(\(((?:[^\}]|\}(?!\}))*?)\))?\s*\}\}/g,
-        template_anotate: /(<\w+)(?=[\s>])(?![^>]*_Template_Item)([^>]*)/g,
+        template_anotate: /(<\w+)(?=[\s>])(?![^>]*_tmplitem)([^>]*)/g,
         text_only_template: /^\s*([^<\s][^<]*)?(<[\w\W]+>)([^>]*[^>\s])?\s*$/,
         html_expr: /^[^<]*(<[\w\W]+>)[^>]*$|\{\{\! |\{\%! /,
         last_word: /\w$/
     };
 
-    function newTemplate_Item(options, parentItem, fn, data) {
+    function newTmplItem(options, parentItem, fn, data) {
         var newItem = {
             data: data || (data === 0 || data === false) ? data : (parentItem ? parentItem.data : {}),
             _wrap: parentItem ? parentItem._wrap : null,
@@ -39,7 +49,7 @@ Coding by Andrew.Kim (E-mail: andrewkim365@qq.com)
             newItem.AKjs_Template = fn;
             newItem._ctnt = newItem._ctnt || $.isFunction(newItem.AKjs_Template) && newItem.AKjs_Template($, newItem) || fn;
             newItem.key = ++itemKey;
-            (stack.length ? wrappedItems : newTemplate_Items)[itemKey] = newItem;
+            (stack.length ? wrappedItems : newTmplItems)[itemKey] = newItem;
         }
         return newItem;
     }
@@ -51,10 +61,10 @@ Coding by Andrew.Kim (E-mail: andrewkim365@qq.com)
         replaceAll: "replaceWith"
     }, function(name, original) {
         $.fn[ name ] = function(selector) {
-            var ret = [], insert = $(selector), elems, i, l, Template_Items,
+            var ret = [], insert = $(selector), elems, i, l, tmplItems,
                 parent = this.length === 1 && this[0].parentNode;
 
-            appendToTemplate_Items = newTemplate_Items || {};
+            appendToTmplItems = newTmplItems || {};
             if(parent && parent.nodeType === 11 && parent.childNodes.length === 1 && insert.length === 1) {
                 insert[ original ](this[0]);
                 ret = this;
@@ -68,9 +78,9 @@ Coding by Andrew.Kim (E-mail: andrewkim365@qq.com)
                 cloneIndex = 0;
                 ret = this.pushStack(ret, name, insert.selector);
             }
-            Template_Items = appendToTemplate_Items;
-            appendToTemplate_Items = null;
-            $.AKjs_Template.complete(Template_Items);
+            tmplItems = appendToTmplItems;
+            appendToTmplItems = null;
+            $.AKjs_Template.complete(tmplItems);
             return ret;
         };
     });
@@ -79,15 +89,17 @@ Coding by Andrew.Kim (E-mail: andrewkim365@qq.com)
         AKjs_Template: function(data, options, parentItem) {
             var ele = $(this[0]);
             ele.addClass("ak-for");
-            var ret = $.AKjs_Template(this[0], data, options, parentItem).appendTo($(this[0]));
-            setTimeout(function() {
+            var ret = $.AKjs_Template(ele, data, options, parentItem);
+            newTmplItem(options.callback(ele,ret));
+            $(function() {
                 ele.removeClass("ak-for");
-                $(ret).eq(0).prev().remove();
-            },100);
-            return ret
+                ele.children().eq(0).remove();
+                ret.appendTo(ele);
+            });
+            return ret;
         },
-        Template_Item: function() {
-            var ret = $.Template_Item(this[0]);
+        tmplItem: function() {
+            var ret = $.tmplItem(this[0]);
             return ret;
         },
         template: function(name) {
@@ -97,10 +109,10 @@ Coding by Andrew.Kim (E-mail: andrewkim365@qq.com)
 
         domManip: function(args, table, callback, options) {
             if(args[0] && $.isArray(args[0])) {
-                var dmArgs = $.makeArray(arguments), elems = args[0], elemsLength = elems.length, i = 0, Template_Item;
-                while(i < elemsLength && !(Template_Item = $.data(elems[i++], "Template_Item"))) {
+                var dmArgs = $.makeArray(arguments), elems = args[0], elemsLength = elems.length, i = 0, tmplItem;
+                while(i < elemsLength && !(tmplItem = $.data(elems[i++], "tmplItem"))) {
                 }
-                if(Template_Item && cloneIndex) {
+                if(tmplItem && cloneIndex) {
                     dmArgs[2] = function(fragClone) {
                         $.AKjs_Template.afterManip(this, fragClone, callback);
                     };
@@ -110,8 +122,8 @@ Coding by Andrew.Kim (E-mail: andrewkim365@qq.com)
                 oldManip.apply(this, arguments);
             }
             cloneIndex = 0;
-            if(!appendToTemplate_Items) {
-                $.AKjs_Template.complete(newTemplate_Items);
+            if(!appendToTmplItems) {
+                $.AKjs_Template.complete(newTmplItems);
             }
             return this;
         }
@@ -121,12 +133,12 @@ Coding by Andrew.Kim (E-mail: andrewkim365@qq.com)
         AKjs_Template: function(AKjs_Template, data, options, parentItem) {
             var ret, topLevel = !parentItem;
             if(topLevel) {
-                parentItem = topTemplate_Item;
+                parentItem = topTmplItem;
                 AKjs_Template = $.template[AKjs_Template] || $.template(null, AKjs_Template);
                 wrappedItems = {};
             } else if(!AKjs_Template) {
                 AKjs_Template = parentItem.AKjs_Template;
-                newTemplate_Items[parentItem.key] = parentItem;
+                newTmplItems[parentItem.key] = parentItem;
                 parentItem.nodes = [];
                 if(parentItem.wrapped) {
                     updateWrapped(parentItem, parentItem.wrapped);
@@ -144,37 +156,36 @@ Coding by Andrew.Kim (E-mail: andrewkim365@qq.com)
             }
             ret = $.isArray(data) ?
                 $.map(data, function(dataItem) {
-                    return dataItem ? newTemplate_Item(options, parentItem, AKjs_Template, dataItem) : null;
+                    return dataItem ? newTmplItem(options, parentItem, AKjs_Template, dataItem) : null;
                 }) :
-                [ newTemplate_Item(options, parentItem, AKjs_Template, data) ];
+                [ newTmplItem(options, parentItem, AKjs_Template, data) ];
             return topLevel ? $(build(parentItem, null, ret)) : ret;
         },
-        Template_Item: function(elem) {
-            var Template_Item;
+        tmplItem: function(elem) {
+            var tmplItem;
             if(elem instanceof $) {
                 elem = elem[0];
             }
-            while(elem && elem.nodeType === 1 && !(Template_Item = $.data(elem,
-                "Template_Item")) && (elem = elem.parentNode)) {
+            while(elem && elem.nodeType === 1 && !(tmplItem = $.data(elem,
+                "tmplItem")) && (elem = elem.parentNode)) {
             }
-            return Template_Item || topTemplate_Item;
+            return tmplItem || topTmplItem;
         },
         template: function(name, AKjs_Template) {
             if(AKjs_Template) {
                 if(typeof AKjs_Template === "string") {
-                    AKjs_Template = buildAKjs_TemplateFn(AKjs_Template)
+                    AKjs_Template = buildTmplFn(AKjs_Template)
                 } else if(AKjs_Template instanceof $) {
                     AKjs_Template = AKjs_Template[0] || {};
                 }
                 if(AKjs_Template.nodeType) {
-                    AKjs_Template = $.data(AKjs_Template, "AKjs_Template") || $.data(AKjs_Template, "AKjs_Template", buildAKjs_TemplateFn(AKjs_Template.innerHTML));
+                    AKjs_Template = $.data(AKjs_Template, "AKjs_Template") || $.data(AKjs_Template, "AKjs_Template", buildTmplFn(AKjs_Template.innerHTML));
                 }
                 return typeof name === "string" ? ($.template[name] = AKjs_Template) : AKjs_Template;
             }
             return name ? (typeof name !== "string" ? $.template(null, name) :
                 ($.template[name] || $.template(null, name))) : null;
         },
-
         encode: function(text) {
             return ("" + text).split("<").join("&lt;").split(">").join("&gt;").split('"').join("&#34;").split("'").join("&#39;");
         }
@@ -221,29 +232,29 @@ Coding by Andrew.Kim (E-mail: andrewkim365@qq.com)
             }
         },
         complete: function(items) {
-            newTemplate_Items = {};
+            newTmplItems = {};
         },
         afterManip: function afterManip(elem, fragClone, callback) {
             var content = fragClone.nodeType === 11 ?
-                $.makeArray(fragClone.childNodes) :
-                fragClone.nodeType === 1 ? [fragClone] : [];
+                $.makeArray(fragClone.childNodes) : fragClone.nodeType === 1 ? [fragClone] : [];
             callback.call(elem, fragClone);
-            storeTemplate_Items(content);
+            storeTmplItems(content);
             cloneIndex++;
         }
     });
-    function build(Template_Item, nested, content) {
+
+    function build(tmplItem, nested, content) {
         var frag, ret = content ? $.map(content, function(item) {
-            return (typeof item === "string") ? (Template_Item.key ? item.replace(regex.template_anotate, "$1 " + AKjs_TemplateItmAtt + "=\"" + Template_Item.key + "\" $2") : item) : build(item, Template_Item, item._ctnt);
-        }) : Template_Item;
+            return (typeof item === "string") ?
+                (tmplItem.key ? item.replace(regex.template_anotate, "$1 " + tmplItmAtt + "=\"" + tmplItem.key + "\" $2") : item) : build(item, tmplItem, item._ctnt);
+        }) : tmplItem;
         if(nested) {
             return ret;
         }
         ret = ret.join("");
         ret.replace(regex.text_only_template, function(all, before, middle, after) {
             frag = $(middle).get();
-
-            storeTemplate_Items(frag);
+            storeTmplItems(frag);
             if(before) {
                 frag = unencode(before).concat(frag);
             }
@@ -259,12 +270,11 @@ Coding by Andrew.Kim (E-mail: andrewkim365@qq.com)
         el.innerHTML = text;
         return $.makeArray(el.childNodes);
     }
-    function buildAKjs_TemplateFn(markup) {
+    function buildTmplFn(markup) {
         var parse_tag = function(all, slash, type, fnargs, target, parens, args) {
             if(!type) {
                 return "');__.push('";
             }
-
             var tag = $.AKjs_Template.tag[ type ], def, expr, exprAutoFnDetect;
             if(!tag && window.console && console.group) {
                 //console.group("Exception");
@@ -298,7 +308,6 @@ Coding by Andrew.Kim (E-mail: andrewkim365@qq.com)
                     .split("$2").join(fnargs || def.$2 || "") +
                 "__.push('";
         };
-
         var depreciated_parse = function() {
             if($.AKjs_Template.tag[arguments[2]]) {
                 //console.group("Depreciated");
@@ -323,8 +332,7 @@ Coding by Andrew.Kim (E-mail: andrewkim365@qq.com)
         return new Function("$", "$item", parsed_markup_data);
     }
     function updateWrapped(options, wrapped) {
-        options._wrap = build(options, true,
-            $.isArray(wrapped) ? wrapped : [regex.html_expr.test(wrapped) ? wrapped : $(wrapped).html()]
+        options._wrap = build(options, true, $.isArray(wrapped) ? wrapped : [regex.html_expr.test(wrapped) ? wrapped : $(wrapped).html()]
         ).join("");
     }
     function unescape(args) {
@@ -335,7 +343,7 @@ Coding by Andrew.Kim (E-mail: andrewkim365@qq.com)
         div.appendChild(elem.cloneNode(true));
         return div.innerHTML;
     }
-    function storeTemplate_Items(content) {
+    function storeTmplItems(content) {
         var keySuffix = "_" + cloneIndex, elem, elems, newClonedItems = {}, i, l, m;
         for(i = 0,l = content.length; i < l; i++) {
             if((elem = content[i]).nodeType !== 1) {
@@ -348,65 +356,61 @@ Coding by Andrew.Kim (E-mail: andrewkim365@qq.com)
             processItemKey(elem);
         }
         function processItemKey(el) {
-            var pntKey, pntNode = el, pntItem, Template_Item, key;
-            if((key = el.getAttribute(AKjs_TemplateItmAtt))) {
-                while(pntNode.parentNode && (pntNode = pntNode.parentNode).nodeType === 1 && !(pntKey = pntNode.getAttribute(AKjs_TemplateItmAtt))) {
+            var pntKey, pntNode = el, pntItem, tmplItem, key;
+            if((key = el.getAttribute(tmplItmAtt))) {
+                while(pntNode.parentNode && (pntNode = pntNode.parentNode).nodeType === 1 && !(pntKey = pntNode.getAttribute(tmplItmAtt))) {
                 }
                 if(pntKey !== key) {
-                    pntNode = pntNode.parentNode ? (pntNode.nodeType === 11 ? 0 : (pntNode.getAttribute(AKjs_TemplateItmAtt) || 0)) : 0;
-                    if(!(Template_Item = newTemplate_Items[key])) {
-                        Template_Item = wrappedItems[key];
-                        Template_Item = newTemplate_Item(Template_Item, newTemplate_Items[pntNode] || wrappedItems[pntNode]);
-                        Template_Item.key = ++itemKey;
-                        newTemplate_Items[itemKey] = Template_Item;
+                    pntNode = pntNode.parentNode ? (pntNode.nodeType === 11 ? 0 : (pntNode.getAttribute(tmplItmAtt) || 0)) : 0;
+                    if(!(tmplItem = newTmplItems[key])) {
+                        tmplItem = wrappedItems[key];
+                        tmplItem = newTmplItem(tmplItem, newTmplItems[pntNode] || wrappedItems[pntNode]);
+                        tmplItem.key = ++itemKey;
+                        newTmplItems[itemKey] = tmplItem;
                     }
                     if(cloneIndex) {
-                        cloneTemplate_Item(key);
+                        cloneTmplItem(key);
                     }
                 }
-                el.removeAttribute(AKjs_TemplateItmAtt);
-            } else if(cloneIndex && (Template_Item = $.data(el, "Template_Item"))) {
-                cloneTemplate_Item(Template_Item.key);
-                newTemplate_Items[Template_Item.key] = Template_Item;
-                pntNode = $.data(el.parentNode, "Template_Item");
+                el.removeAttribute(tmplItmAtt);
+            } else if(cloneIndex && (tmplItem = $.data(el, "tmplItem"))) {
+                cloneTmplItem(tmplItem.key);
+                newTmplItems[tmplItem.key] = tmplItem;
+                pntNode = $.data(el.parentNode, "tmplItem");
                 pntNode = pntNode ? pntNode.key : 0;
             }
-            if(Template_Item) {
-                pntItem = Template_Item;
+            if(tmplItem) {
+                pntItem = tmplItem;
                 while(pntItem && pntItem.key != pntNode) {
                     pntItem.nodes.push(el);
                     pntItem = pntItem.parent;
                 }
-                delete Template_Item._ctnt;
-                delete Template_Item._wrap;
-                $.data(el, "Template_Item", Template_Item);
+                delete tmplItem._ctnt;
+                delete tmplItem._wrap;
+                $.data(el, "tmplItem", tmplItem);
             }
-            function cloneTemplate_Item(key) {
+            function cloneTmplItem(key) {
                 key = key + keySuffix;
-                Template_Item = newClonedItems[key] =
-                    (newClonedItems[key] || newTemplate_Item(Template_Item,
-                        newTemplate_Items[Template_Item.parent.key + keySuffix] || Template_Item.parent));
+                tmplItem = newClonedItems[key] =
+                    (newClonedItems[key] || newTmplItem(tmplItem,
+                        newTmplItems[tmplItem.parent.key + keySuffix] || tmplItem.parent));
             }
         }
     }
-
     function tiCalls(content, AKjs_Template, data, options) {
         if(!content) {
             return stack.pop();
         }
         stack.push({ _: content, AKjs_Template: AKjs_Template, item:this, data: data, options: options });
     }
-
     function tiNest(AKjs_Template, data, options) {
         return $.AKjs_Template($.template(AKjs_Template), data, options, this);
     }
-
     function tiWrap(call, wrapped) {
         var options = call.options || {};
         options.wrapped = wrapped;
         return $.AKjs_Template($.template(call.AKjs_Template), call.data, options, call.item);
     }
-
     function tiHtml(filter, textOnly) {
         var wrapped = this._wrap;
         return $.map(
@@ -417,7 +421,6 @@ Coding by Andrew.Kim (E-mail: andrewkim365@qq.com)
                     e.outerHTML || outerHtml(e);
             });
     }
-
     function tiUpdate() {
         var coll = this.nodes;
         $.AKjs_Template(null, null, null, this).insertBefore(coll[0]);
