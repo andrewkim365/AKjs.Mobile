0728e89162c1f03956dfc2b7f3a62b2f6198567e
andrewkim365@qq.com <andrewkim365@qq.com>
2018-10-15 11:03:20 +0800

    Add files via upload

--- plugin/AKjs_Slider.js
+++ plugin/AKjs_Slider.js
@@ -1,31 +1,33 @@
 ﻿/*
-Modification Date: 2018-09-21
+Modification Date: 2018-10-15
 Coding by Andrew.Kim (E-mail: andrewkim365@qq.com)
 */
 /*-----------------------------------------------AKjs_Slider------------------------------------------*/
 (function($){
     var AKjs_Slider = function(ele, opt) {
         var self = this;
-        self.$element = ele,
-            self.defaults = {
-                fullpage: false,
-                UpDown: false,
-                start: 1,
-                speed: 500,
-                interval: 5000,
-                autoPlay: false,
-                loopPlay: true,
-                dotShow: true,
-                arrShow: true,
-                dotClass:"",
-                arrClass:"",
-                CustomHeight: false,
-                ActiveClass: "bg_title",
-                afterSlider: function() {}
-            },
-            self.clickable = true,
-            self.options = $.extend({},
-                self.defaults, opt)
+        self.$element = ele;
+        self.defaults = {
+            fullpage: false,
+            UpDown: false,
+            start: 1,
+            SlideAni: false,
+            speed: 500,
+            interval: 5000,
+            touchPlay: true,
+            autoPlay: false,
+            HoverPause: true,
+            loopPlay: true,
+            dotShow: true,
+            arrShow: true,
+            dotClass:"",
+            arrClass:"",
+            CustomHeight: false,
+            ActiveClass: "bg_title",
+            afterSlider: function() {}
+        };
+        self.clickable = true;
+        self.options = $.extend({}, self.defaults, opt);
     };
     AKjs_Slider.prototype = {
         init: function() {
@@ -198,12 +200,14 @@ Coding by Andrew.Kim (E-mail: andrewkim365@qq.com)
                     self.moveTo(index, "ak-arr_next");
                 };
                 timer = setInterval(play, self.options.interval);
-                ele.hover(function() {
-                        timer = clearInterval(timer)
-                    },
-                    function() {
-                        timer = setInterval(play, self.options.interval)
-                    })
+                if (self.options.HoverPause) {
+                    ele.hover(function () {
+                            timer = clearInterval(timer)
+                        },
+                        function () {
+                            timer = setInterval(play, self.options.interval)
+                        });
+                }
             }
             if (self.options.fullpage) {
                 ele.on({
@@ -218,89 +222,17 @@ Coding by Andrew.Kim (E-mail: andrewkim365@qq.com)
                 ele.css({"height": win_h});
             }
             AKjs_UserAgent();
-            SliderLi.on({
-                touchstart: function(es) {
-                    touchStartY = es.originalEvent.touches[0].clientY;
-                    touchStartX = es.originalEvent.touches[0].clientX;
-                },
-                touchend: function(es) {
-                    var touchEndY = es.originalEvent.changedTouches[0].clientY,
-                        touchEndX = es.originalEvent.changedTouches[0].clientX,
-                        yDiff = touchStartY - touchEndY,
-                        xDiff = touchStartX - touchEndX;
-                    if (self.options.UpDown) {
-                        if (Math.abs(xDiff) < Math.abs(yDiff)) {
-                            if (yDiff > 10) {
-                                if (index >= SliderSize) {
-                                    if (self.options.loopPlay) {
-                                        index = 1;
-                                        self.moveTo(index, "ak-arr_next");
-                                    }
-                                } else {
-                                    index += 1;
-                                    self.moveTo(index, "ak-arr_next");
-                                }
-                            } else {
-                                if (index == 1) {
-                                    if (self.options.loopPlay) {
-                                        index = SliderSize;
-                                        self.moveTo(index, "ak-arr_prev");
-                                    }
-                                } else {
-                                    index -= 1;
-                                    self.moveTo(index, "ak-arr_prev");
-                                }
-                            }
-                        }
-                    } else {
-                        if (Math.abs(xDiff) > Math.abs(yDiff)) {
-                            if (xDiff > 10) {
-                                if (index >= SliderSize) {
-                                    if (self.options.loopPlay) {
-                                        index = 1;
-                                        self.moveTo(index, "ak-arr_next");
-                                    }
-                                } else {
-                                    index += 1;
-                                    self.moveTo(index, "ak-arr_next");
-                                }
-                            } else {
-                                if (index == 1) {
-                                    if (self.options.loopPlay) {
-                                        index = SliderSize;
-                                        self.moveTo(index, "ak-arr_prev");
-                                    }
-                                } else {
-                                    index -= 1;
-                                    self.moveTo(index, "ak-arr_prev");
-                                }
-                            }
-                        }
-                    }
-                    touchStartY = null;
-                    touchStartX = null
-                },
-                touchmove: function(es) {
-                    var touchEndY = es.originalEvent.changedTouches[0].clientY,
-                        touchEndX = es.originalEvent.changedTouches[0].clientX,
-                        yDiff = touchStartY - touchEndY,
-                        xDiff = touchStartX - touchEndX;
-                    if (self.options.UpDown) {
-                        es.preventDefault();
-                    }
-                }
-            });
-            if (!IsMobile) {
+            if (self.options.touchPlay) {
                 SliderLi.on({
-                    mousedown: function(es) {
-                        mouseStartY = es.clientY;
-                        mouseStartX = es.clientX;
+                    touchstart: function(es) {
+                        touchStartY = es.originalEvent.touches[0].clientY;
+                        touchStartX = es.originalEvent.touches[0].clientX;
                     },
-                    mouseup: function(es) {
-                        var mouseEndY = es.screenY,
-                            mouseEndX = es.screenX,
-                            yDiff = mouseStartY - mouseEndY,
-                            xDiff = mouseStartX - mouseEndX;
+                    touchend: function(es) {
+                        var touchEndY = es.originalEvent.changedTouches[0].clientY,
+                            touchEndX = es.originalEvent.changedTouches[0].clientX,
+                            yDiff = touchStartY - touchEndY,
+                            xDiff = touchStartX - touchEndX;
                         if (self.options.UpDown) {
                             if (Math.abs(xDiff) < Math.abs(yDiff)) {
                                 if (yDiff > 10) {
@@ -350,11 +282,86 @@ Coding by Andrew.Kim (E-mail: andrewkim365@qq.com)
                                 }
                             }
                         }
-                        mouseStartY = null;
-                        mouseStartX = null
+                        touchStartY = null;
+                        touchStartX = null
+                    },
+                    touchmove: function(es) {
+                        var touchEndY = es.originalEvent.changedTouches[0].clientY,
+                            touchEndX = es.originalEvent.changedTouches[0].clientX,
+                            yDiff = touchStartY - touchEndY,
+                            xDiff = touchStartX - touchEndX;
+                        if (self.options.UpDown) {
+                            es.preventDefault();
+                        }
                     }
                 });
+                if (!IsMobile) {
+                    SliderLi.on({
+                        mousedown: function(es) {
+                            mouseStartY = es.clientY;
+                            mouseStartX = es.clientX;
+                        },
+                        mouseup: function(es) {
+                            var mouseEndY = es.screenY,
+                                mouseEndX = es.screenX,
+                                yDiff = mouseStartY - mouseEndY,
+                                xDiff = mouseStartX - mouseEndX;
+                            if (self.options.UpDown) {
+                                if (Math.abs(xDiff) < Math.abs(yDiff)) {
+                                    if (yDiff > 10) {
+                                        if (index >= SliderSize) {
+                                            if (self.options.loopPlay) {
+                                                index = 1;
+                                                self.moveTo(index, "ak-arr_next");
+                                            }
+                                        } else {
+                                            index += 1;
+                                            self.moveTo(index, "ak-arr_next");
+                                        }
+                                    } else {
+                                        if (index == 1) {
+                                            if (self.options.loopPlay) {
+                                                index = SliderSize;
+                                                self.moveTo(index, "ak-arr_prev");
+                                            }
+                                        } else {
+                                            index -= 1;
+                                            self.moveTo(index, "ak-arr_prev");
+                                        }
+                                    }
+                                }
+                            } else {
+                                if (Math.abs(xDiff) > Math.abs(yDiff)) {
+                                    if (xDiff > 10) {
+                                        if (index >= SliderSize) {
+                                            if (self.options.loopPlay) {
+                                                index = 1;
+                                                self.moveTo(index, "ak-arr_next");
+                                            }
+                                        } else {
+                                            index += 1;
+                                            self.moveTo(index, "ak-arr_next");
+                                        }
+                                    } else {
+                                        if (index == 1) {
+                                            if (self.options.loopPlay) {
+                                                index = SliderSize;
+                                                self.moveTo(index, "ak-arr_prev");
+                                            }
+                                        } else {
+                                            index -= 1;
+                                            self.moveTo(index, "ak-arr_prev");
+                                        }
+                                    }
+                                }
+                            }
+                            mouseStartY = null;
+                            mouseStartX = null
+                        }
+                    });
+                }
             }
+
         },
         moveTo: function(index, dir) {
             var self = this,
@@ -372,33 +379,51 @@ Coding by Andrew.Kim (E-mail: andrewkim365@qq.com)
                 if (dir == "ak-arr_prev") {
                     offset = -1 * offset
                 }
-                if (self.options.UpDown) {
-                    sliderInder.children(".dis_block_im").stop().animate({
-                            top: -offset
-                        },
-                        self.options.speed,
-                        function() {
-                            $(this).removeClass("dis_block_im")
-                        });
-                    SliderLi.eq(index - 1).css("top", offset + "px").addClass("dis_block_im").stop().animate({
-                            top: 0
-                        },
-                        self.options.speed,
-                        function() {
-                            self.clickable = true
-                        });
+                if (self.options.SlideAni) {
+                    if (self.options.UpDown) {
+                        sliderInder.children(".dis_block_im").stop().animate({
+                                top: -offset
+                            },
+                            self.options.speed,
+                            function() {
+                                $(this).removeClass("dis_block_im")
+                            });
+                        SliderLi.eq(index - 1).css("top", offset + "px").addClass("dis_block_im").stop().animate({
+                                top: 0
+                            },
+                            self.options.speed,
+                            function() {
+                                self.clickable = true
+                            });
+                    } else {
+                        sliderInder.children(".dis_block_im").stop().animate({
+                                left: -offset
+                            },
+                            self.options.speed,
+                            function() {
+                                $(this).removeClass("dis_block_im")
+                            });
+                        SliderLi.eq(index - 1).css("left", offset + "px").addClass("dis_block_im").stop().animate({
+                                left: 0
+                            },
+                            self.options.speed,
+                            function() {
+                                self.clickable = true
+                            });
+                    }
                 } else {
                     sliderInder.children(".dis_block_im").stop().animate({
-                            left: -offset
-                        },
-                        self.options.speed,
-                        function() {
-                            $(this).removeClass("dis_block_im")
-                        });
-                    SliderLi.eq(index - 1).css("left", offset + "px").addClass("dis_block_im").stop().animate({
                             left: 0
-                        },
-                        self.options.speed,
+                        }, self.options.speed,
+                        function() {
+                            var _this = $(this);
+                            setTimeout(function () {
+                                _this.removeClass("dis_block_im zindex_2");
+                            },self.options.speed);
+                        });
+                    SliderLi.eq(index - 1).addClass("dis_block_im zindex_2").stop().animate({
+                            left: 0
+                        },self.options.speed,
                         function() {
                             self.clickable = true
                         });
@@ -413,7 +438,7 @@ Coding by Andrew.Kim (E-mail: andrewkim365@qq.com)
                         ele.css({"height": SliderLi.eq(index - 1).outerHeight()});
                     }
                 }
-                self.options.afterSlider(index);
+                self.options.afterSlider(index,SliderLi.eq(index - 1));
                 dots_li.removeClass(self.options.ActiveClass);
                 dots_li.eq(index - 1).addClass(self.options.ActiveClass);
             } else {
